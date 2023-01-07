f5c063dc53dd6bded5d6a5020a3fe798390686b3
Rating001 <106230549+Rating001@users.noreply.github.com>
2023-01-06 21:41:54 -0800

    Added the Properties to the Blog Class.

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -1,6 +1,33 @@
-﻿namespace CoderThoughtsBlog.Models
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoderThoughtsBlog.Models
 {
     public class Blog
     {
+        public int Id { get; set; }
+        public string AuthorId { get; set; }
+        [Required]
+        [StringLength(100,ErrorMessage = "Your blog {0} must be between {2} and {1} characters in length.", MinimumLength = 2)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(500, ErrorMessage = "Your blog {0} must be between {2} and {1} characters in length.", MinimumLength = 2)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
+        public DateTime Created { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated Date")]
+        public DateTime? Updated { get; set; }
+
+        [Display(Name = "Blog Image")]
+        public byte[] ImageData { get; set; }
+        [Display(Name = "Image Type")]
+        public string ContentType { get; set; }
+        [NotMapped]
+        public IFormFile Image { get; set; }
     }
 }
