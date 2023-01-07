d7a354226d419638371745a9d6c386470408bb12
Rating001 <106230549+Rating001@users.noreply.github.com>
2023-01-06 21:51:57 -0800

    Input the properties for the Post Class.

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,6 +1,41 @@
-﻿namespace CoderThoughtsBlog.Models
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoderThoughtsBlog.Models
 {
     public class Post
     {
+        public int Id { get; set; }
+
+        [Display(Name = "Blog Name")]
+        public int BlogId { get; set; }
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(200, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        public string Abstract { get; set; }
+        [Required]
+        public string Content { get; set; }
+        
+        [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
+        public DateTime Created { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated Date")]
+        public DateTime? Updated { get; set; }
+
+        public string Slug { get; set; }
+        
+        public byte[] ImageData { get; set; }
+        public string ContentType { get; set; }
+
+        [NotMapped]
+        public IFormFile Image { get; set; }
     }
 }
