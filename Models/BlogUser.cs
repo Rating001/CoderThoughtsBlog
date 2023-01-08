6e4948c2108bfa4bcd01009e49d42c4544812603
Rating001 <106230549+Rating001@users.noreply.github.com>
2023-01-08 13:31:42 -0800

    Created the BlogUser Class which inherits the IdentityUser Model. Added properties to BlogUser including ICollections.

---
+++ Models/BlogUser.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoderThoughtsBlog.Models
+{
+    public class BlogUser : IdentityUser
+    {
+        [Required]
+        [StringLength(30,ErrorMessage="The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public byte[] ImageData { get; set; }
+        public string ContentType { get; set; }
+        //public IFormFile ImageFile { get; set; }
+
+        [StringLength(100, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        public string? FacebookUrl { get; set; }
+        [StringLength(100, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        public string? TwitterUrl { get; set; }
+        [StringLength(3000, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        public string? UserSignature { get; set; }
+
+        [NotMapped]
+        public string FullName 
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        //Navigation/Collection Properties
+
+        public virtual ICollection<Blog> Blogs { get; set; } = new HashSet<Blog>();
+        public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
+
+
+    }
+}
