ff9fac49c61a45bf08b34fc60850294ade81b34c
Rating001 <106230549+Rating001@users.noreply.github.com>
2023-01-13 21:27:01 -0800

    Configured the MailKit and completed the setup for the IBlogEmailSender (Interface). Tested the email send with a "Resend confirmation email" which was successful.

---
+++ Services/EmailService.cs
@@ -0,0 +1,52 @@
+﻿using CoderThoughtsBlog.Services.Interfaces;
+using CoderThoughtsBlog.ViewModels;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using Org.BouncyCastle.Asn1.Smime;
+using System.Threading.Tasks;
+
+namespace CoderThoughtsBlog.Services
+{
+    public class EmailService : IBlogEmailSender
+    {
+        private readonly MailSettings _mailSettings;
+
+        public EmailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+        public Task SendContactEmailAsync(string emailFrom, string name, string subject, string htmlMessage)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
+        {
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Email);
+            email.To.Add(MailboxAddress.Parse(emailTo));
+            email.Subject = subject;
+
+            //var builder = new BodyBuilder();
+            //builder.HtmlBody = htmlMessage;
+            var builder = new BodyBuilder()
+            {
+                HtmlBody = htmlMessage
+            };
+
+            email.Body = builder.ToMessageBody();
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.MailHost, _mailSettings.MailPort, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Email, _mailSettings.MailPassword);
+
+            await smtp.SendAsync(email);
+
+            smtp.Disconnect(true);
+
+        }
+    }
+}
